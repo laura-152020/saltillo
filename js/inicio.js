6b5bd6722e9eca554d9dbb51896ec6390fcb18b2
laura-152020 <104606089+laura-152020@users.noreply.github.com>
2024-03-13 15:11:11 -0500

    tacos al pastor

--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,18 @@
+
+/* IMAGEN TACOS CLICK*/
+// Obtener la imagen de "Tacos Al Pastor" por su clase o id
+const imageTacosAlPastor = document.querySelector('.image-wrapper img');
+
+// Agregar un evento de clic a la imagen
+imageTacosAlPastor.addEventListener('click', function() {
+    // Redirigir a la página "tacos_al_pastor.html" al hacer clic en la imagen
+    window.location.href = 'tacos_al_pastor.html';
+});
+
+
+
+
+/*NAVEGADOR*/
 const list = document.querySelectorAll('.list');
 
 function activeLink() {
@@ -8,3 +23,4 @@ function activeLink() {
 
 list.forEach((item) =>
 item.addEventListener('click',activeLink));
+
